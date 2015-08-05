6e24a512fd4cc0154e9eb114363ffc2e39f3fc38
Sylvain Tenier <sylvain.tenier@gmail.com>
2015-08-06 01:05:53 +0200

    plot 2 code & resulting graph

---
+++ plot2.R
@@ -0,0 +1,25 @@
+#only load data if the object does not exist - avoids downloading and parsing the file if it has already been done in another script
+#this code could be in another R file sourced by each plot*.R file, but the asignment asks for 4 R files only!
+
+if(!exists("elecData")){
+    #download resource file using curl (Mac/Linux). Remove method="curl" on windows if download fails
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="elecdata.zip", method="curl")
+
+    ""
+    #find where the 2880 observations for the 2 days we are interested in (1440 minutes/day) are located in the file
+    goodlines<-grep("^((1|2)/2/2007)+",readLines(unz("elecdata.zip", "household_power_consumption.txt")))
+    #get column names from the file first line
+    noms <- strsplit(readLines(unz("elecdata.zip", "household_power_consumption.txt"),1),";")[[1]]
+    #read from file
+    elecData <- read.table(unz("elecdata.zip", "household_power_consumption.txt"),sep=";",skip=goodlines[1]-1,nrows=length(goodlines),col.names=noms,na.strings="?")
+    #convert in R date format
+    elecData$Date <- as.Date(strptime(elecData$Date,format="%d/%m/%Y"))
+}
+
+
+##specific code for plot 2
+png("plot2.png", width = 480, height = 480, units = "px")
+with(elecData,plot(Global_active_power,type="l",xaxt='n',xlab="",ylab="Global Active Power (kilowatts)"))
+axis(1,at=c(0,length(goodlines)/2,length(goodlines)),labels=c("Thu", "Fri", "Sat"))
+dev.off()
+
